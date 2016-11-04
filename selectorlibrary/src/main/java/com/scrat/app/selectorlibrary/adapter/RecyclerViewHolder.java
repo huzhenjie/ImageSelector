f3e05ac791c4207c9479332ac0b44f508a31f33f
HuZhenJie <scrat_zhenjie@hotmail.com>
2016-11-04 18:10:05 +0800

    remove unuse method

--- selectorlibrary/src/main/java/com/scrat/app/selectorlibrary/adapter/RecyclerViewHolder.java
+++ selectorlibrary/src/main/java/com/scrat/app/selectorlibrary/adapter/RecyclerViewHolder.java
@@ -1,11 +1,8 @@
 package com.scrat.app.selectorlibrary.adapter;
 
-import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.util.SparseArray;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -30,25 +27,11 @@ public class RecyclerViewHolder extends RecyclerView.ViewHolder {
         return (T) view;
     }
 
-    public Context getContext() {
-        return mConvertView.getContext();
-    }
-
     public View getRootView() {
         return mConvertView;
     }
 
     public RecyclerViewHolder setText(int viewId, CharSequence content) {
-        TextView view = getView(viewId);
-        view.setText(content);
-        return this;
-    }
-
-    public RecyclerViewHolder setText(int viewId, CharSequence content, int maxLength) {
-        if (content != null && content.length() > maxLength) {
-            content = content.subSequence(0, maxLength) + "...";
-        }
-
         TextView view = getView(viewId);
         view.setText(content);
         return this;
@@ -60,58 +43,8 @@ public class RecyclerViewHolder extends RecyclerView.ViewHolder {
         return this;
     }
 
-    public RecyclerViewHolder setVisibility(int viewId, int visible) {
-        View view = getView(viewId);
-        view.setVisibility(visible);
-        return this;
-    }
-
-    public RecyclerViewHolder setBackgroundColor(int viewId, int color) {
-        View view = getView(viewId);
-        view.setBackgroundColor(color);
-        return this;
-    }
-
-    public RecyclerViewHolder setBackground(int viewId, Drawable background) {
-        View view = getView(viewId);
-
-        if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.JELLY_BEAN) {
-            view.setBackgroundDrawable(background);
-        } else {
-            view.setBackground(background);
-        }
-        return this;
-    }
-
-    public RecyclerViewHolder setImageDrawable(ImageView imageView, int drawableId) {
-        imageView.setImageDrawable(mConvertView.getContext().getResources().getDrawable(drawableId));
-        return this;
-    }
-
-    public RecyclerViewHolder setBackground(int viewId, int drawableId) {
-        Drawable drawable = mConvertView.getContext().getResources().getDrawable(drawableId);
-        return setBackground(viewId, drawable);
-    }
-
     public RecyclerViewHolder setOnClickListener(View.OnClickListener l) {
         mConvertView.setOnClickListener(l);
-        return this;
-    }
-
-    public RecyclerViewHolder setOnLongClickListener(View.OnLongClickListener l) {
-        mConvertView.setOnLongClickListener(l);
-        return this;
-    }
-
-    public RecyclerViewHolder setOnClickListener(int viewId, View.OnClickListener l) {
-        View view = getView(viewId);
-        view.setOnClickListener(l);
-        return this;
-    }
-
-    public RecyclerViewHolder setOnLongClickListener(int viewId, View.OnLongClickListener l) {
-        View view = getView(viewId);
-        view.setOnLongClickListener(l);
         return this;
     }
 
